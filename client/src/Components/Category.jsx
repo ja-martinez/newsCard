58b75b10c27461e9509444bad7b7e3b6aa50896d
Alejandro Martinez <j.alejandro.mrtnz@gmail.com>
2019-07-25 14:22:44 -0700

    finish styling for the section navbar

--- client/src/Components/Category.jsx
+++ client/src/Components/Category.jsx
@@ -5,19 +5,19 @@ class Category extends React.Component {
     render () {
         const categories = this.props.categories.map((cat, index) => {
             return <button 
-                className="categoryButton"
+                className="section-nav-links"
                 key={index}
                 id={index}
                 value={cat}
                 onClick={this.props.handleButtonSortByCategory}>{cat}</button>
+
+
         })
 
         return (
-            <div>
-                <div className="categoryContainer">
-                    {categories}
-                </div>
-            </div>
+            <nav className="section-nav links">
+                {categories}
+            </nav>
         )
     }
 }
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -35,40 +35,6 @@ class Header extends React.Component {
             </Link>
           </div>
         </nav>
-        <nav className="navbar">
-          <a className="navbar-brand" href="/">
-            <h3 className="logo">NEWSCARD</h3>
-          </a>
-          <ul className="nav justify-content-end">
-            <li className="nav-item">
-              <Link
-                to="/"
-                style={{ padding: "10px", color: "white" }}
-                className="routerLink"
-              >
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/savedArticles"
-                style={{ padding: "10px", color: "white" }}
-                className="routerLink"
-              >
-                Saved Articles
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/login"
-                style={{ color: "white" }}
-                className="routerLink"
-              >
-                LogIn
-              </Link>
-            </li>
-          </ul>
-        </nav>
 
         <Route path="/" exact component={Home} />
         <Route path="/savedArticles" component={SavedArticles} />
